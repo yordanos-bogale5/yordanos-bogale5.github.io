bab874777b9385a48333c836323cb80485ce0ae2
yordanos-bogale5 <bogaleyordanos64@gmail.com>
2023-12-22 00:25:07 +0300

    home screen

---
+++ lib/alters/string_switch.dart
@@ -0,0 +1,28 @@
+String hellotag = '''Hi there, Welcome to My World  ''';
+
+String yourname = "I'm Yordanos Bogale,";
+
+
+String animationtxt1 = " Mobile Application Developer";
+String animationtxt2 = " UI/UX Designer";
+String animationtxt3 = " Web Developer";
+
+
+String contactHeadding = "Let’s try my service now!";
+String contactSubHeadding =
+    "Let’s work together and make everything super cute and super useful.";
+
+String miniDescription =
+    "Freelancer providing services for programming and design content needs. Join me down below and let's get started!";
+
+String servicesSubHeading =
+    "Since the beginning of my journey as a freelance designer and developer, I've worked in projects and collaborated with talented people to create digital products for both business and consumer use.";
+
+
+String protfolioSubHeading =
+    "Since the beginning of my journey as a designer and developer, I have created digital products for business and consumer use. This is a little bit.";
+const String aboutMeHeadline =
+    'I\'m Yordanos Bogale, a Mobile App developer, Web developer and UI designer.';
+
+const String aboutMeDetail =
+    'I\'m studying Software Engineering at ArbaMinch University, Ethiopia. I have been developing mobile apps for over 1 years now. I have worked in teams for various projects and got valuable learning experience.';
---
+++ lib/functions/api/media_links.dart
@@ -0,0 +1,21 @@
+// const List<String> socialIconURL = [
+//   "https://img.icons8.com/fluency/48/000000/facebook-new.png",
+//   // "https://img.icons8.com/color/48/000000/instagram-new--v1.png",
+//   "https://img.icons8.com/color/48/000000/youtube-play.png",
+//   "https://img.icons8.com/fluency/48/000000/twitter.png",
+//   "https://img.icons8.com/color/48/000000/linkedin-circled--v1.png",
+
+//   "https://img.icons8.com/fluency/48/000000/github.png",
+// ];
+
+class IconUrls {
+  static String upworkUrl =
+      "https://img.icons8.com/external-tal-revivo-shadow-tal-revivo/48/000000/external-upwork-a-global-freelancing-platform-where-professionals-connect-and-collaborate-remotely-logo-shadow-tal-revivo.png";
+
+  static String whatsappUrl =
+      "https://img.icons8.com/color/48/000000/whatsapp--v1.png";
+
+  static String fiverrUrl = "https://img.icons8.com/color/48/000000/fiverr.png";
+
+
+}
